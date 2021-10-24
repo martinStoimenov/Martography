591fc48cfdd48c0583231fc0ce141c2fc507aa50
Martin <martin.hj15@gmail.com>
2021-10-25 00:48:15 +0300

    add blog views and visual tweaks

---
+++ Martography/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Martography.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ById()
+        {
+            return View();
+        }
+    }
+}
